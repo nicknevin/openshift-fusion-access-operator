bcb6e196acfcec2b3949b1b7a97e049ff884a4e7
Jonathan Kilzi <jkilzi@redhat.com>
2025-09-11 16:38:04 +0300

    feat: add Daemon schema and integrate health check in FileSystemsCreateButton
    - Introduced a new JSON schema for the Daemon API to define its structure and properties.
    - Implemented a health check for the Daemon in the FileSystemsCreateButton component, disabling the button if the Daemon is not healthy and providing a tooltip with an error message.
    - Updated the useFileSystemTableRowViewModel hook to comment out the description logic temporarily.
    - Added a custom hook, useWatchDaemon, to monitor the Daemon's status.
    
    This enhances the user experience by ensuring that file system creation is only possible when the underlying infrastructure is ready.
    
    Addresses:
    - OCPNAS-184
    - OCPNAS-257
    - OCPNAS-259
    
    Signed-off-by: Jonathan Kilzi <jkilzi@redhat.com>

--- console/src/features/file-systems/components/FileSystemsCreateButton.tsx
+++ console/src/features/file-systems/components/FileSystemsCreateButton.tsx
@@ -1,18 +1,53 @@
-import { Button } from "@patternfly/react-core";
+import { Button, Tooltip } from "@patternfly/react-core";
 import type { ButtonProps } from "@patternfly/react-core/dist/js/components/Button";
 import { useFusionAccessTranslations } from "@/shared/hooks/useFusionAccessTranslations";
+import { useWatchDaemon } from "@/shared/hooks/useWatchDaemon";
+import { useEffect, useRef, useState } from "react";
 
 type CreateFileSystemButtonProps = Omit<ButtonProps, "variant">;
 
 export const FileSystemsCreateButton: React.FC<CreateFileSystemButtonProps> = (
   props
 ) => {
+  const { isDisabled, ...otherProps } = props;
   const { t } = useFusionAccessTranslations();
 
+  const [isDaemonHealthy, setIsDaemonHealthy] = useState(false);
+  const tooltipRef = useRef<HTMLButtonElement>(null);
+
+  const daemon = useWatchDaemon();
+
+  useEffect(() => {
+    if (daemon.loaded && Array.isArray(daemon.data) && daemon.data.length > 0) {
+      const [daemonData] = daemon.data;
+      const daemonStatus =
+        daemonData.status?.conditions
+          ?.filter((condition) => condition.type == "Healthy")
+          .filter((condition) => condition.status === "True") ?? [];
+
+      setIsDaemonHealthy(daemonStatus.length === 1);
+    }
+  }, [daemon.loaded, daemon.data, isDaemonHealthy]);
+
   return (
-    <Button {...props} variant="primary">
-      {t("Create file system")}
-    </Button>
+    <>
+      <Button
+        aria-describedby="create-file-system-tooltip"
+        {...otherProps}
+        isDisabled={isDisabled || !isDaemonHealthy}
+        variant="primary"
+        ref={tooltipRef}
+      >
+        {t("Create file system")}
+      </Button>
+      {!isDaemonHealthy && (
+        <Tooltip
+          id="create-file-system-tooltip"
+          content={t("CNSA infrastructure is not ready")}
+          triggerRef={tooltipRef}
+        />
+      )}
+    </>
   );
 };
 
